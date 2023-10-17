a171244e5bceb33276e7a28a16f4eafbebe7d661
aurelia-mp <aurelia.porolli@gmail.com>
2023-10-17 10:58:20 -0300

    responsive

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import Main from './components/Main/Main';
 import { BrowserRouter } from 'react-router-dom'
 import './App.css';
 import { SessionContextProvider } from './context/sessionContext';
+import { useMediaQuery } from 'react-responsive'
+
 
 function App() {
-  return (
-      <BrowserRouter>
-        <Header />
-        <SessionContextProvider>
-          <Main />
-        </SessionContextProvider>
-        <Footer />
-      </BrowserRouter>
 
+  const esComputadora = useMediaQuery({ query: '(min-width: 1224px)'})
+  return (
+      <>
+         {!esComputadora ? 
+        <BrowserRouter>
+          <Header />
+          <SessionContextProvider>
+            <Main />
+          </SessionContextProvider>
+          <Footer />
+        </BrowserRouter>
+        :
+        <div className="computadora">
+        <img src="/fotos/logo.jpg"></img>
+        <h2>This app was developped for smaller screens.<br></br>Please open on a mobile phone o tablet</h2>
+        <p><a href="https://www.palosantohotel.com">Browse our website </a></p>
+        </div>
+        }
+      </> 
   );
 }
 
